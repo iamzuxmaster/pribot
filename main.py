422bbab95241d65769f159dc4b0e4782e5924cd0
Sabitovich Sardorbek <sardorbek31@mail.ru>
2022-01-03 23:03:01 +0500

    0.011

--- main.py
+++ main.py
@@ -1,23 +1,37 @@
 # Local
 import buttons as btn
 import strings
-from app import BOT_TOKEN, SERVER_HOST, SERVER_REQUEST_GET, SERVER_REQUEST_POST
+from app import BOT_TOKEN, SERVER_HOST, GET, POST
+import genius
 # Global 
 from aiogram import types,Bot, Dispatcher, executor
 import json
 
-dp = Dispatcher(Bot(BOT_TOKEN, parse_mode=types.message.ParseMode.HTML))
+bot = Bot(BOT_TOKEN, parse_mode=types.message.ParseMode.HTML)
+dp = Dispatcher(bot)
 
+user = {}
+session = {}
 def touched(user_language):
     with open('titles/strings.json', 'r', encoding='UTF-8') as file:
         data = json.load(file)[user_language]
     return data
 
+def sttrs(user_languge):
+    main_menu = ''
+    return {'main_menu': main_menu}
+
+def keys(user_language):
+    main_menu = ''
+    return {'main_menu': main_menu}
+
+# Start
 @dp.message_handler(commands=['start'])
 async def get_start(message: types.Message):
     chat_id = message.chat.id
     pass
 
+# Help - information
 @dp.message_handler(commands=['help'])
 async def get_help(message: types.Message):
     chat_id = message.chat.id
@@ -32,3 +46,5 @@ async def get_message(message: types.Message):
 async def get_inline_message(callback: types.CallbackQuery):
     chat_id = callback.message.chat.id
     pass
+
+executor.start_polling(dp)
---
+++ strings.py
@@ -0,0 +1,10 @@
+import requests
+
+
+def select_language():
+    # Please note Unused language
+    text = "🇺🇿 Iltimos tilni tanlang..."
+    text += "🇷🇺 Пожалуйста выберите язык"
+    text += "🇺🇸 Please select a language"
+    # ? Ex: text += "🇹🇷 Lütfen bir dil seçin" 
+    return text
